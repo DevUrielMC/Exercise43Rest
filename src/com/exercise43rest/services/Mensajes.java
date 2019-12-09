d8491ab2b12c737ad3e58e70953767bf9298c89a
DevUrielMC <urimarcervantes.95@gmail.com>
2019-12-09 13:27:44 -0600

    Realizando prueba uno de REST

--- src/com/exercise43rest/services/Mensajes.java
+++ src/com/exercise43rest/services/Mensajes.java
@@ -5,7 +5,10 @@ import javax.ws.rs.Path;
 
 @Path("/Mensajes")					//http://localhost:8080/ esto es el dominio exercise43rest/rest path para acceder a la clase /Mensajes
 public class Mensajes {
+	//"Con este path el endpoint se transforma en:"
+	//http://localhost:8080/Exercise34Rest/Mensajes/diHola
 	
+	@Path("/diHola")
 	@GET
 	public String sayHello() 
 	{
