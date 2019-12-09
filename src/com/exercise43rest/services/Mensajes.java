7a6a75b9320a0b53cddf9c086fcc2a1d05135bba
DevUrielMC <urimarcervantes.95@gmail.com>
2019-12-09 12:56:13 -0600

    Realizando prueba uno de REST

--- src/com/exercise43rest/services/Mensajes.java
+++ src/com/exercise43rest/services/Mensajes.java
@@ -1,5 +1,16 @@
 package com.exercise43rest.services;
 
-public class Mensajes {
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 
+@Path("/Mensajes")					//http://localhost:8080/ esto es el dominio exercise43rest/rest path para acceder a la clase /Mensajes
+public class Mensajes {
+	
+	@GET
+	public String sayHello() 
+	{
+		return "Hi Knee Breacker";
+	}
+
+	
 }
